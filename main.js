1cd854e87493b091037ef18ad29b5053d9e26d5e
kevin-et-holmes <kevinholmes921@gmail.com>
2024-03-14 10:24:13 -0700

    fixing input bugs

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for (let key of keys) {
             display_output.innerHTML = "";
         } else if (value == "backspace") {
             input = input.slice(0, -1);
-            display_input.innerHTML = input;
+            display_input.innerHTML = CleanInput(input);
         } else if (value == "=") {
             let result = eval(input);
 
@@ -23,13 +23,36 @@ for (let key of keys) {
             if (input.indexOf("(") == -1 || input.indexOf("(") != -1 && input.indexOf(")") != -1 && input.lastIndexOf("(") < input.lastIndexOf(")")) {
                 input += "("
             } else if (input.indexOf("(") != -1 && input.indexOf(")") == -1 || input.indexOf("(") != -1 && input.indexOf(")") != -1 && input.lastIndexOf("(") >input.lastIndexOf(")")) {
-                input -= ")"
+                input += ")"
             }
-            display_input.innerHTML = input;
+            display_input.innerHTML = CleanInput(input);
         } else {
             input += value;
-            display_input.innerHTML = input;
+            display_input.innerHTML = CleanInput(input);
         }
-        
-})
+    })
+}
+
+function CleanInput (input) {
+    let input_array = input.split("");
+    let input_array_length = input_array.length;
+
+    for (let i = 0; i < input_array_length; i++) {
+        if (input_array[i] == "*") {
+            input_array[i] = ` <span class="operator">x</span> `;
+        } else if (input_array[i] == "/") {
+            input_array[i] = ` <span class="operator">÷</span> `;
+        } else if (input_array[i] == "+") {
+            input_array[i] = ` <span class="operator">+</span> `;
+        } else if (input_array[i] == "-") {
+            input_array[i] = ` <span class="operator">-</span> `;
+        } else if (input_array[i] == "(") {
+            input_array[i] = ` <span class="brackets">(</span> `;
+        } else if (input_array[i] == ")") {
+            input_array[i] = ` <span class="brackets">)</span> `;
+        } else if (input_array[i] == "%") {
+            input_array[i] = ` <span class="percent">%</span> `;
+        }
+    }
+    return input_array.join("")
 }
